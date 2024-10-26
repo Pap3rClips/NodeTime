ff920b507d58dc4e45c2b66ba74e702407994406
null <null@null.com>
2024-10-26 18:39:30 +0200

    error handling version

--- weatherly/index.js
+++ weatherly/index.js
@@ -1,23 +1,20 @@
-const parameter = {
-    access_key: "6c9cb537aafe7484fcc2006a72df69cb",
-    query: "Paris"
-}
 const request = require("postman-request")
-request(`http://api.weatherstack.com/current?access_key=${parameter.access_key}&query=${parameter.query}`, function (error, response, body){
-    const {location, current} = JSON.parse(body)
-    console.log(location)
-    console.log(current)
-    console.log(`
-        Pays : ${location.country}
-        Regions : ${location.region}
-        Nom : ${location.name}
-        Latitude : ${location.lat}
-        Longitude : ${location.lon}
-        `)
-    console.log(`
-        Température : ${current.temperature}
-        Précipitations : ${current.precip}
-        Humidité : ${current.humidity}%
-        Vent : ${current.wind_speed}km/h
-        `)
+
+const parameter = {
+    api_key : "6c9cb537aafe7484fcc2006a72df69cb",
+    place : "Dublin"
+}
+
+const url = `http://api.weatherstack.com/current?access_key=${parameter.api_key}&query=${parameter.place}`
+
+request(url, function(error, response, body) {
+    const content = JSON.parse(body)
+    if (content.success === false)
+    {
+        console.log(content.error.info)
+    }
+    else
+    {
+        console.log(`Il fait ${content.current.temperature} degré a ${content.location.name} (${content.location.region},${content.location.country})`)
+    }
 })
