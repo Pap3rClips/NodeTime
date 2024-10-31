f483459bbb1ea24cfb093eaabe8be7c6d3d9f8a4
null <null@null.com>
2024-10-31 19:54:17 +0100

    expresso edit

--- expresso/index.js
+++ expresso/index.js
@@ -1,15 +1,7 @@
-const express = require("express")
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req,res) => {
-    res.send('Racine')
-})
-
-app.get('/about', (req, res) => {
-    res.send('About me')
-})
-
-app.listen(port, () => {
-    console.log("going ...")
-})
+app.get('/', (req, res) => {app.send('Racine');})
+app.get('/about', (req, res) => {app.send('About me');})
+app.listen(port, () => {console.log(`listening on port ${port}...`)})
